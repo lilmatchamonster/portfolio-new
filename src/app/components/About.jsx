6f3b7ca5cddaa1c2f08ac562485535be48adef84
Ashley Owens <ash4741@gmail.com>
2024-08-23 11:14:22 -0400

    Remove Unneeded Code + Small CSS Adjustment

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,6 +1,5 @@
 "use client"
 import Image from "next/image";
-import styled from 'styled-components';
 import profilePic2 from '../imgs/profilePic2.jpg';
 import leaf from '../imgs/leaf.webp';
 import leaf_invert from '../imgs/leaf-inverted.webp';
@@ -18,79 +17,10 @@ const theme = {
   },
 };
 
-// const AboutStyles = styled.div`
-//   /* box-sizing: border-box;
-//   background-position: center;
-//   -webkit-background-size: cover;
-//   -moz-background-size: cover;
-//   -o-background-size: cover;
-//   background-size: cover;
-//   background-repeat: no-repeat;
-//   background-attachment: fixed; */
-//   padding-top: 3.5%;
-//   /* position: absolute; */
-//   top: 0;
-//   bottom: 0;
-//   width: 100%;
-//   @media screen and (max-width: 800px) {
-//     padding-top: 9%;
-//   }
-
-//   .intro-card{
-//     display: flex;
-//     align-items: center;
-//     padding-left: 10%;
-//     background-color: hsla(351, 28%, 52%, .09);
-//     @media screen and (max-width: 750px) {
-//       padding-left: 0;
-//       flex-direction: column;
-//     }
-    
-//     .img {
-//       box-shadow: 5px 10px 18px silver;
-//       width: 15rem;
-//       height: 15rem;
-//       border-radius: 20%;
-//       /* border-radius: 50%; */
-//       margin: 2.5%;
-//     }
-//     p {
-//       background-color: hsla(351, 28%, 52%, .09);
-//       box-shadow: 5px 10px 18px silver;
-//       font-size: 1rem;
-//       font-weight: 500;
-//       text-align: left;
-//       text-shadow: 1px 1px silver;
-//       padding: 2%;
-//       margin: 1%;
-//       width: 40%;
-//       @media screen and (max-width: 750px) {
-//         background-color: hsla(000, 0%, 100%, .7);
-//         transition: background-color .75s;
-//         width: 60%;
-//       }
-//       @media screen and (max-width: 360px) {
-//         width: 96%;
-//       }
-//     }
-//     .contact {
-//       margin-bottom: 5%;
-//     }
-//   }
-//   .preview {
-//     img {
-//       width: 200px;
-//       margin: 1%;
-//       border: 1px solid white;
-//     }
-//   }
-// `;
-
 export default function About() {
   return (
     <div className="about-container">
       <Image className='bg-img' src={leaf} alt='leaf' fill={true} priority={true} />
-      {/* <AboutStyles style={theme['a']}> */}
       <div className="about-styles" style={theme['a']}>
         <h1 style={{ color: 'hsla(351, 28%, 52%, .19)', textShadow: '0 0 3px hsl(195,16%,85%,.3)' }}>
           About
--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import styled from 'styled-components';
 import herokuLogo from '../imgs/herokuLogo.svg';
 import sqliteLogo from '../imgs/sqliteLogo.svg';
 import restfulLogo from '../imgs/restfulLogo.svg';
@@ -13,12 +12,11 @@ import beach_invert from '../imgs/beach-close-up-cold-inverted.webp';
 import "./skills.css";
 
 library.add(fab);
-//! Icon styles loading slowly, investigate possible css icon usage
+
 const mySkills = [
   {
     name: 'HTML5',
-    // icon: <FontAwesomeIcon icon={['fab', 'html5']} />,
-    icon: <FontAwesomeIcon icon='fa-brands fa-html5' />,
+    icon: <FontAwesomeIcon icon={['fab', 'html5']} />,
     description:
       'Experience using HTML to seperate web elements into appropriate HTML tags and display data.',
   },
@@ -50,8 +48,7 @@ const mySkills = [
     name: 'RESTful Api',
     icon: null,
     class: 'rest',
-    // newIcon: <img src={`${restfulLogo}`} width='100px' alt='{REST}' />,
-    newIcon: <Image src={restfulLogo} width={100} alt='{REST}' />,
+    newIcon: <Image src={restfulLogo} width={100} alt='REST' />,
     description:
       'Experience in working with and building RESTFul API endpoints to build full CRUD functionality',
   },
@@ -76,14 +73,6 @@ const mySkills = [
     name: 'SQLite3',
     icon: null,
     class: 'sql',
-    // newIcon: (
-    //   <img
-    //     src={`${sqliteLogo}`}
-    //     width='50px'
-    //     className={'icon'}
-    //     alt='SQLite3'
-    //   />
-    // ),
     newIcon: (
       <Image
         src={sqliteLogo}
@@ -97,16 +86,7 @@ const mySkills = [
   {
     name: 'Heroku',
     icon: null,
-    // class: 'restful fix',
     class: 'heroku',
-    // newIcon: (
-    //   <img
-    //     src={`${herokuLogo}`}
-    //     width='40px'
-    //     className={'icon heroku'}
-    //     alt='Heroku'
-    //   />
-    // ),
     newIcon: (
       <Image
         src={herokuLogo}
@@ -115,7 +95,7 @@ const mySkills = [
       />
     ),
     description:
-    'Experience with live deployment/configuration using heroku linked with a github repository master branch',
+    'Experience with live deployment/ configuration using heroku linked with a github repository master branch',
   },
   {
     name: 'LESS',
@@ -127,20 +107,10 @@ const mySkills = [
     name: 'Styled Components',
     icon: null,
     class: 'styled-com',
-    // newIcon: (
-    //   <img
-    //     src={`${styledComponentsLogo}`}
-    //     width='48px'
-    //     height="100%"
-    //     className={'icon'}
-    //     alt='Styled Components'
-    //   />
-    // ),
     newIcon: (
       <Image
         src={styledComponentsLogo}
         width={48}
-        // height="100%"
         alt='Styled Components'
       />
     ),
@@ -148,92 +118,6 @@ const mySkills = [
       'Experience using styled components to not only create themes and easy to handle dynamic styling, but also to create reusable custom styled components.',
   },
 ];
-
-// const SkillsStyles = styled.div`
-//   box-sizing: border-box;
-//   background-position: center;
-//   -webkit-background-size: cover;
-//   -moz-background-size: cover;
-//   -o-background-size: cover;
-//   background-size: cover !important;
-//   background-repeat: no-repeat;
-//   background-attachment: fixed !important;
-//   padding-top: 3.5%;
-//   position: fixed;
-//   top: 0;
-//   bottom: 0;
-//   right: 0;
-//   left: 0;
-//   height: 100%;
-//   @media screen and (max-width: 800px) {
-//     padding-top: 9%;
-//   }
-  
-//   .skills-container {
-//     display: flex;
-//     width: 100%;
-//     height:60%;
-//     margin-top: 2.5%;
-//   }
-//   .skills {
-//     overflow-y: scroll;
-//     display: flex;
-//     flex-wrap: wrap;
-//     justify-content: center;
-//     padding: 1%;
-//     background-color: hsl(195,16%,85%, .2);
-//     width: 100%;
-//     height: 100%;
-
-//     .card {
-//       border: 1px solid white;
-//       background-color: hsl(195,16%,85%, .7);
-//       margin: .5%;
-//       padding: 1%;
-//       font-size: 3rem;
-//       width: 20%;
-//       @media screen and (max-width: 900px) {
-//         width: 40%;
-//       }
-//       @media screen and (max-width: 500px) {
-//         width: 80%;
-//         margin: 1rem;
-//       }
-      
-//       p {
-//         font-size: 1rem;
-//         color: #0c2b31;  
-//         font-weight: 500;        
-//         text-shadow: 1px 1px silver;
-//       }
-//     }
-//     .restful {
-//       font-size: 1.51rem !important;
-//       margin:14px 0 34px;
-//       /* color: #0c2b31;  */
-//       color: #6D6F83 !important; 
-//       pointer-events: none;
-//       -webkit-user-select: none;
-//       -khtml-user-select: none;
-//       -moz-user-select: none;
-//       -o-user-select: none;
-//       user-select: none;
-//     }
-//     .fix {
-//       margin-bottom: 7%;
-//     }
-//     .rest {
-//       margin-bottom: 5%;
-//     }
-//     .sql {
-//       margin-bottom: -2.5%;
-//     }
-//     .heroku {
-//       margin-bottom: -3.3%;
-//     }
-
-//   }
-// `;
 
 const theme = {
   a: {
@@ -247,41 +131,34 @@ const theme = {
 export default function Skills() {
   return (
     <div>
-        {/* <SkillsStyles style={theme['a']}> */}
-        <div className="skills-styles" style={theme['a']}>
-          <h1 style={{ color: 'hsl(195,16%,85%,.17)', textShadow: '0 0 3px hsl(195,16%,85%,.3)' }}>
-            Tech Stack
-          </h1>
-          <div className={'skills-container'}>
-            <div className={'skills'}>
-              {mySkills.map((skill, i) => (
-                <div className={'card'} key={i}>
-                  {skill.icon !== null ? (
-                    <>
-                      {skill.icon}
-                      <p className='name'>{skill.name}</p>
-                      <p>
-                        {skill.description}
-                      </p>
-                    </>
-                  ) : (
-                    <>
-                      {/* <p className={`${skill.class}`}>
-                        {skill.newIcon}
-                      </p> */}
-                      {/* <span className={`${skill.class}`}> */}
-                        {skill.newIcon}
-                      {/* </span> */}
-                      <p className={`${skill.class}`}>{skill.name}</p>
-                      {/* <p >{skill.name}</p> */}
-                      <p>{skill.description}</p>
-                    </>
-                  )}
-                </div>
-              ))}
-            </div>
+      <div className="skills-styles" style={theme['a']}>
+        <h1 style={{ color: 'hsl(195,16%,85%,.17)', textShadow: '0 0 3px hsl(195,16%,85%,.3)' }}>
+          Tech Stack
+        </h1>
+        <div className={'skills-container'}>
+          <div className={'skills'}>
+            {mySkills.map((skill, i) => (
+              <div className={'card'} key={i}>
+                {skill.icon !== null ? (
+                  <>
+                    {skill.icon}
+                    <p className='name'>{skill.name}</p>
+                    <p>
+                      {skill.description}
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    {skill.newIcon}
+                    <p className={`${skill.class}`}>{skill.name}</p>
+                    <p>{skill.description}</p>
+                  </>
+                )}
+              </div>
+            ))}
           </div>
         </div>
+      </div>
 		</div>
   );
 }
