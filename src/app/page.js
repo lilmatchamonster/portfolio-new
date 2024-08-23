c40c13af4246c313adc7040de692bf8ec34baf7d
Ashley Owens <ash4741@gmail.com>
2024-08-23 13:53:29 -0400

    Remove Img from About and Update Component Stacking

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
+import Image from "next/image";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import leaf from './imgs/leaf.webp';
 import s from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={s.main}>
+      <Image className={s.bgimg} src={leaf} alt='leaf' fill={true} priority={true} />
       <Nav />
-      {/* <About /> */}
+      <About />
       <Skills />
     </main>
   );
